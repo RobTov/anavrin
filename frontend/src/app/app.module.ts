f09447d2c93a1fcf1562706e53a0cdc122f892f2
RobTov <tovelrob25@gmail.com>
2022-04-08 19:57:40 -0400

    angular initial template

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,14 +1,35 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { CoursesComponent } from './components/courses/courses.component';
+import { HabitsComponent } from './components/habits/habits.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { NavmenuComponent } from './components/navmenu/navmenu.component';
+
+
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'courses', component: CoursesComponent },
+  { path: 'routines', component: HabitsComponent },
+  { path: 'profile', component: ProfileComponent },
+]
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CoursesComponent,
+    HabitsComponent,
+    ProfileComponent,
+    NavmenuComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ frontend/src/app/components/habits/habits.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-habits',
+  templateUrl: './habits.component.html',
+  styleUrls: ['./habits.component.scss']
+})
+export class HabitsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
